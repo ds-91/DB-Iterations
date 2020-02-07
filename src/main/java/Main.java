5366a7eeec7a613204efc5cffbb444c77e7b9630
drk7891 <37281604+drk7891@users.noreply.github.com>
2020-02-06 19:43:09 -0500

    added functions instead of in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,27 +5,63 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
+
   private static final String CSV_FILE_PATH = "SEOExample.csv";
 
   public static void main(String[] args) {
-    try {
-      FileReader fr = new FileReader(CSV_FILE_PATH);
-      CSVReader reader = new CSVReader(fr);
+    FileReader f = createReader(CSV_FILE_PATH);
+    CSVReader r = new CSVReader(f);
 
+    //csvPrintAll(f, r);
+    csvPrintByArrayIndex(f, r);
+  }
+
+  public static FileReader createReader(String filename) {
+    FileReader fr;
+    try {
+      fr = new FileReader(filename);
+    } catch (FileNotFoundException e) {
+      return null;
+    }
+    return fr;
+  }
+
+  public static void csvPrintAll(FileReader f, CSVReader r) {
+    try {
       String[] nextLine;
 
-      while ((nextLine = reader.readNext()) != null) {
+      while ((nextLine = r.readNext()) != null) {
         for (String line : nextLine) {
           System.out.print(line);
         }
         System.out.println();
       }
-    } catch (FileNotFoundException e) {
-      System.out.println("file not found");
     } catch (CsvValidationException e) {
       System.out.println("something about validation");
+      e.printStackTrace();
     } catch (IOException e) {
       System.out.println("io exception");
+      e.printStackTrace();
+    }
+  }
+
+  public static void csvPrintByArrayIndex(FileReader f, CSVReader r) {
+    try {
+      String[] nextLine;
+      while ((nextLine = r.readNext()) != null) {
+        /**
+         * Array indexes:
+         * 0 = name, 1 = date, 2 = entry point, 3 = region, 4 = location
+         * 5 = accepted, 6 = enrolled, 7 = search terms, 8 = engine, 9 = reason
+         */
+        System.out.println(nextLine[0]);
+      }
+    } catch (CsvValidationException e) {
+      System.out.println("something about validation");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("io exception");
+      e.printStackTrace();
     }
   }
 }
