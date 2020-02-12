f9bf5b8a04c8e77af866a24d8f162e270e5ba9d0
drk7891 <37281604+drk7891@users.noreply.github.com>
2020-02-12 17:18:25 -0500

    refactor into class

---
+++ src/main/java/CSVParser.java
@@ -0,0 +1,56 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVParser {
+
+  public CSVParser(String input) throws IOException, CsvValidationException {
+    if (fileExists(input)) {
+      //csvPrintAll(input);
+      csvPrintByArrayIndex(input);
+    }
+  }
+
+  private void csvPrintAll(String filename)
+      throws IOException, CsvValidationException {
+    String[] nextLine;
+
+    FileReader f = new FileReader(filename);
+    CSVReader r = new CSVReader(f);
+
+    while ((nextLine = r.readNext()) != null) {
+      for (String line : nextLine) {
+        System.out.println(line);
+      }
+      System.out.println();
+    }
+  }
+
+  public static void csvPrintByArrayIndex(String filename)
+      throws IOException, CsvValidationException {
+
+    FileReader f = new FileReader(filename);
+    CSVReader r = new CSVReader(f);
+    String[] nextLine;
+    while ((nextLine = r.readNext()) != null) {
+      /**
+       * Array indexes:
+       * 0 = name, 1 = date, 2 = entry point, 3 = region, 4 = location
+       * 5 = accepted, 6 = enrolled, 7 = search terms, 8 = engine, 9 = reason
+       */
+      System.out.println(nextLine[0]);
+    }
+  }
+
+  private boolean fileExists(String filename) {
+    File f = new File(filename);
+
+    if (!f.exists()) {
+      System.out.println("File doesn't exist!");
+      return false;
+    }
+    return true;
+  }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,12 @@
-import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
 
   private static final String CSV_FILE_PATH = "SEOExample.csv";
 
-  public static void main(String[] args) {
-    FileReader f = createReader(CSV_FILE_PATH);
-    CSVReader r = new CSVReader(f);
-
-    //csvPrintAll(f, r);
-    csvPrintByArrayIndex(f, r);
-  }
-
-  public static FileReader createReader(String filename) {
-    FileReader fr;
-    try {
-      fr = new FileReader(filename);
-    } catch (FileNotFoundException e) {
-      return null;
-    }
-    return fr;
-  }
-
-  public static void csvPrintAll(FileReader f, CSVReader r) {
-    try {
-      String[] nextLine;
-
-      while ((nextLine = r.readNext()) != null) {
-        for (String line : nextLine) {
-          System.out.print(line);
-        }
-        System.out.println();
-      }
-    } catch (CsvValidationException e) {
-      System.out.println("something about validation");
-      e.printStackTrace();
-    } catch (IOException e) {
-      System.out.println("io exception");
-      e.printStackTrace();
-    }
-  }
-
-  public static void csvPrintByArrayIndex(FileReader f, CSVReader r) {
-    try {
-      String[] nextLine;
-      while ((nextLine = r.readNext()) != null) {
-        /**
-         * Array indexes:
-         * 0 = name, 1 = date, 2 = entry point, 3 = region, 4 = location
-         * 5 = accepted, 6 = enrolled, 7 = search terms, 8 = engine, 9 = reason
-         */
-        System.out.println(nextLine[0]);
-      }
-    } catch (CsvValidationException e) {
-      System.out.println("something about validation");
-      e.printStackTrace();
-    } catch (IOException e) {
-      System.out.println("io exception");
-      e.printStackTrace();
-    }
+  public static void main(String[] args)
+      throws IOException, CsvValidationException {
+    CSVParser p = new CSVParser(CSV_FILE_PATH);
   }
 }
